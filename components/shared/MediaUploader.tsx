da3257db3da322db144951d8663bc166b995781e
Usef-Ali <usifali99@gmail.com>
2024-08-30 04:20:41 +0300

    first commit

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_Mendr"
+      uploadPreset="Mendr"
       options={{
         multiple: false,
         resourceType: "image",
